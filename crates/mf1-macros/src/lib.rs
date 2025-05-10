bb14afe1b0ea35e0c68739fc47051d658bfac826
Jade Ellis <jade@ellis.link>
2025-05-10 19:24:06 +0100

    chore: More explicitly transform to proc_macro2 tokens

--- crates/mf1-macros/src/lib.rs
+++ crates/mf1-macros/src/lib.rs
@@ -1,3 +1,4 @@
+use proc_macro2;
 use t_macro::OutputType;
 
 mod load_locales;
@@ -12,8 +13,9 @@ pub fn load_locales(_tokens: proc_macro::TokenStream) -> proc_macro::TokenStream
 }
 #[proc_macro]
 pub fn t_l_string(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    match t_macro::t_macro(tokens.into(), OutputType::String) {
-        Ok(ts) => ts.into(),
+    let tokens_2 = proc_macro2::TokenStream::from(tokens);
+    match t_macro::t_macro(tokens_2, OutputType::String) {
+        Ok(ts) => proc_macro::TokenStream::from(ts),
         Err(err) => err.into(),
     }
 }
